db3c6fd35c34ea99069d08be287ce0b24da51938
Olina <alina.usupova2007@gmail.com>
2026-03-09 20:08:39 +0500

    Метод getMinrun

--- src/main/java/ru/itis/aisd501/Timsort.java
+++ src/main/java/ru/itis/aisd501/Timsort.java
@@ -1,4 +1,20 @@
 package ru.itis.aisd501;
 
 public class Timsort {
+    public static void main(String[] args) {
+
+    }
+
+    public static int getMinrun(int n) {
+        // n = N, размер входного массива.
+        int r = 0;
+        // 2^6 = 64.
+        while (n >= 64) {
+            r |= (n & 1);
+            // Если среди младших битов n имеется хотя бы один ненулевой бит, переменная r станет равна 1.
+            n >>= 1;
+        }
+        // Теперь переменная n содержит старшие 6 бит N.
+        return n + r; // minrun
+    }
 }
