3912a476867affb75a4919cccacb66744c2f1657
Olina <alina.usupova2007@gmail.com>
2026-03-14 16:31:57 +0500

    методы findRun и reverse

--- src/main/java/ru/itis/aisd501/Timsort.java
+++ src/main/java/ru/itis/aisd501/Timsort.java
@@ -17,4 +17,34 @@ public class Timsort {
         // Теперь переменная n содержит старшие 6 бит N.
         return n + r; // minrun
     }
+
+    //поиск правой границы, индекса подмассивов
+    static int findRun(int[] arr, int start, int n) {
+        int end = start + 1;
+        if (end == n) return end;
+
+        //если элементы идут в порядке убывания, то меняем местами
+        if (arr[end] < arr[start]) {
+            //ищем индекс, до которого элементы идут в порядке убывания
+            while (end < n && arr[end] < arr[end - 1])
+                end++;
+            reverse(arr, start, end - 1);
+        } else {
+            //ищем индекс, до которого порядок не нарушется
+            while (end < n && arr[end] >= arr[end - 1])
+                end++;
+        }
+        return end;
+    }
+
+    //обратный порядок массива с l до r
+    static void reverse(int[] arr, int l, int r) {
+        while (l < r) {
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
 }
