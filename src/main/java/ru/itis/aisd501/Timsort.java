f6f1d798c9021c0e271ab6302b1dfa6d7da95270
Olina <alina.usupova2007@gmail.com>
2026-03-14 17:06:57 +0500

    метод merge и слияние run'ов в методе timsort

--- src/main/java/ru/itis/aisd501/Timsort.java
+++ src/main/java/ru/itis/aisd501/Timsort.java
@@ -1,15 +1,12 @@
 package ru.itis.aisd501;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Timsort {
-    public static void main(String[] args) {
-
-    }
-
     //расчёт минимального размера подмассива, т.е. minRun
-    static int getMinrun(int n) {
+    public static int getMinrun(int n) {
         // n = N, размер входного массива.
         int r = 0;
         // 2^6 = 64.
@@ -23,7 +20,7 @@ public class Timsort {
     }
 
     //основной метод сортировки
-    static void timsort(int[] arr) {
+    public static void timsort(int[] arr) {
         int n = arr.length;
         int minRun = getMinrun(n);
         List<int[]> runs = new ArrayList<>();
@@ -43,11 +40,43 @@ public class Timsort {
             //текущий индекс равен индексу последнего элемента последнего run'а
             i = runEnd;
 
+            //слияние двух массивов в один в этом же цикле
+            while (runs.size() > 1) {
+                //берёт последние два массива
+                int[] run1 = runs.get(runs.size() - 2);
+                int[] run2 = runs.get(runs.size() - 1);
+
+                //l и r - левая и правая границы, левый и правый индексы соответсвенно
+                int l1 = run1[0], r1 = run1[1];
+                int l2 = run2[0], r2 = run2[1];
+                int len1 = r1 - l1, len2 = r2 - l2;
+
+                if (len1 <= len2) {
+                    merge(arr, l1, r1 - 1, r2 - 1);
+                    runs.remove(runs.size() - 1);
+                    //добавляет в runs слившиеся два массива
+                    //удалили последний массив, а оставшийся(т.е. предпоследний, т.к. индексы сместились)
+                    // превратили в результат - новый слившийся из двух массивов массив
+                    runs.set(runs.size() - 1, new int[]{l1, r2});
+                } else break;
+            }
+        }
+
+        //слияние оставшихся run'ов
+        while (runs.size() > 1) {
+            int[] run1 = runs.get(runs.size() - 2);
+            int[] run2 = runs.get(runs.size() - 1);
+
+            int l1 = run1[0], r1 = run1[1];
+            int l2 = run2[0], r2 = run2[1];
+            merge(arr, l1, r1 - 1, r2 - 1);
+            runs.remove(runs.size() - 1);
+            runs.set(runs.size() - 1, new int[]{l1, r2});
         }
     }
 
     //поиск правой границы, индекса подмассивов
-    static int findRun(int[] arr, int start, int n) {
+    public static int findRun(int[] arr, int start, int n) {
         int end = start + 1;
         if (end == n) return end;
 
@@ -66,7 +95,7 @@ public class Timsort {
     }
 
     //обратный порядок массива с l до r
-    static void reverse(int[] arr, int l, int r) {
+    public static void reverse(int[] arr, int l, int r) {
         while (l < r) {
             int temp = arr[l];
             arr[l] = arr[r];
@@ -77,7 +106,7 @@ public class Timsort {
     }
 
     //сортировка вставками для небольших массивов
-    static void insertionSort(int[] arr, int left, int right) {
+    public static void insertionSort(int[] arr, int left, int right) {
         for (int i = left + 1; i <= right; i++) {
             int key = arr[i];
             int j = i - 1;
@@ -87,5 +116,20 @@ public class Timsort {
             }
             arr[j + 1] = key;
         }
+    }
+
+    //слияние просиходит в исходном массиве!
+    public static void merge(int[] arr, int l, int m, int r) {
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = 0, j = 0, k = l;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) arr[k++] = left[i++];
+            else arr[k++] = right[j++];
+        }
+
+        while (i < left.length) arr[k++] = left[i++];
+        while (j < right.length) arr[k++] = right[j++];
     }
 }
